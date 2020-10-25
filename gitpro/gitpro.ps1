eb3363b5e77509cad9c8904212f6f2455b0fa01a
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-10-25 21:50:59 +0200

    trying to commit everything

--- gitpro/gitpro.ps1
+++ gitpro/gitpro.ps1
@@ -52,4 +52,34 @@ doc/*.txt
 doc/**/*.pdf
 
 # you want to know exactly what you changed, not just which files were changed — you can use the 
-git diff 
+git diff # see all unstaged changes
+
+#  git diff --cached to see what you’ve staged so far
+git diff --cached
+
+# Run git difftool --tool-help to see what is available on your system
+git difftool --tool-help
+
+# then i can commit or set a check point if all is well
+git commit -m "message"
+
+# Adding the -a option to the git commit command makes Git automatically stage every file that is already tracked before doing the commit, letting you skip
+# the git add part:
+git commit -a -m "masseg"
+
+# remove a file
+git rm
+
+# remove a staged file
+git rm --cached fileName
+
+# You can pass files, directories, and file-glob patterns to the git rm command. That means you can do things such as:
+git rm log/\*.log
+#  This command removes all files that have the .log extension in the log/ directory
+
+# rename a file
+git mv file_from file_to
+
+# see what has happened with the file 
+git log
+# PAGE 39
