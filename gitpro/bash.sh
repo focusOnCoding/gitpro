bf7a6b34740f787c2f1173e2c548fbfd41e4a3a1
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-11-06 08:29:30 +0200

    learnt about sudo ids file pamissions and changing files

--- gitpro/bash.sh
+++ gitpro/bash.sh
@@ -1,2 +1,25 @@
 # combining commands
  ls -al / > lsOutPut.tx
+
+ # working with users
+ sudo -s # always super user
+ exit # back to reguler user
+
+ # change to another user
+ su - siyabonga # change to siyabongs home dir
+
+ #! deanguros 
+ su - # home/root.
+
+ # get more inforemation about users
+ id 
+
+ # File Pamissions 
+ ls -l # show long list
+
+ # if i wrote a bash script from a file 
+ # and now what to make that a executeble exe id do the below
+chmod +X file2.txt # make this file exe
+
+# MAKE MYSELF THE ONLY PERSON WHO CAN USE THE FILE 
+chomd 700 file2.txt
