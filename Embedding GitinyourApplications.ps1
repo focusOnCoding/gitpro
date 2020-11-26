7c6215f81376e89454b8271a55e3a239e6c4e39f
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-11-26 14:31:10 +0200

    i completed the progit book but there is still a lot of work to be done

---
+++ Embedding GitinyourApplications.ps1
@@ -0,0 +1,10 @@
+<# Appendix B: Embedding Git in your
+Applications
+If your application is for developers, chances are good that it could benefit from integration with
+source control. Even non-developer applications, such as document editors, could potentially
+benefit from version-control features, and Git’s model works very well for many different
+scenarios.
+If you need to integrate Git with your application, you have essentially two options: spawn a shell
+and call the git command-line program, or embed a Git library into your application. Here we’ll
+cover command-line integration and several of the most popular embeddable Git libraries.#>
+
